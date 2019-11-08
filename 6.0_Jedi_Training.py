acde78f7cdd8a0f74fa4373815844665693ca668
barleypeg <barleypeg@urbandaleschools.com>
2019-11-08 14:24:51 -0600

    stuff

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -20,6 +20,7 @@ Sign your name: Peggy Barley
 
 5.) Convert Hexadecimal #FAAFBD to RGB.
 
+        (250, 175, 189)
 
 6.) Convert Octal 70 to Binary.
 
@@ -27,7 +28,7 @@ Sign your name: Peggy Barley
 
 7.) Convert RGB (32,128,64) to Hexadecimal.
 
-
+        208040
 
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
 
